32a49abc48fdb9a3f7111b27e129186461c87a67
raviranjan940 <raviranjandav940@gmail.com>
2023-10-26 14:22:07 +0530

    my-calc added
    comitted

---
+++ script.js
@@ -0,0 +1,55 @@
+
+// var wrapper = document.getElementById("wrapper");
+
+// let inputField = document.createElement("input");
+// inputField.type = "text";
+// inputField.id = "display";
+
+// wrapper.appendChild(inputField);
+
+let currDisp = '';
+document.querySelector('#display').value = currDisp;
+
+let resultDisp=false;
+
+function addToDisp(value) {
+  if (currDisp === "" || resultDisp) {
+    currDisp = value;
+  } else {
+    currDisp += value;
+  }
+  resultDisp=false;
+  updateDisplay();
+}
+
+function updateDisplay() {
+  const dispElement = document.querySelector('#display');
+  dispElement.textContent = currDisp;
+}
+
+// var currDisp = document.querySelector('#display').value; --> Its inside the every function
+
+function clearDisplay() {
+  currDisp = '';
+  updateDisplay()
+}
+
+function addRemove() {
+  currDisp = currDisp.slice(0, -1);
+  if (currDisp === "") {
+    currDisp = "";
+  }
+  updateDisplay()
+}
+
+function addEqualTo() {
+  let result = eval(currDisp);
+  currDisp += "\n" + result.toString();
+  updateDisplay();
+}
+
+// Attach handleOverflow to window resize event
+//window.addEventListener("resize", handleOverflow);
+
+// Call handleOverflow initially to handle any overflow on page load
+//handleOverflow();
