40c09684f3673ad6d7359874d3989ec823c8f1e2
Ravi Ranjan <67729699+raviranjan940@users.noreply.github.com>
2023-11-28 02:36:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.querySelector('#display').value = currDisp;
 
 let resultDisp=false;
 function addToDisp(value) {
-  if (value === '.' && currDisp.includes('.')) return;
+  if (value === "." && currDisp.includes(".")) return;
   if (currDisp === "" || resultDisp) {
     currDisp = value;
   } else {
